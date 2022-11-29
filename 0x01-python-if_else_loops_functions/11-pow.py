84b5b5ebac5700f26d61bf420939a1823ace8ae4
winnie644 <fridahkemunto09@gmail.com>
2022-11-29 08:25:14 -0800

    11-pow.py

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-for i in range(ord('z'), ord('a') - 1, -1):
+for i in range(ord('Z'), ord('A')-1, -1):
     if i % 2 == 0:
-        diff = 0
-    else:
-        diff = 32
-    print('{}'.format(chr(i - diff)), end='')
+        i = i + 32
+    print("{:c}".format(i), end='')
